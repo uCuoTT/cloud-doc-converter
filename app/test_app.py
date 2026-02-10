6831b0a95610f2f327ec6f626591140659759a4b
Marco Zeolla <zeollamarco@gmail.com>
2026-02-10 18:31:24 +0100

    Fix: Corretto comando esecuzione test nella CI

--- app/test_app.py
+++ app/test_app.py
@@ -9,12 +9,12 @@ class BasicTests(unittest.TestCase):
         app.testing = True
         self.app = app.test_client()
 
-    # 2. Il Test vero e proprio: Controlla se la Homepage risponde
+    # 2. Controlla se la Homepage risponde
     def test_home_page_loads(self):
         # Finge di andare su "/" (la home)
         response = self.app.get('/')
         
-        # Verifica che il codice di risposta sia 200 (OK)
+        # Verifica che il codice di risposta sia 200 
         self.assertEqual(response.status_code, 200)
         
         # Verifica che nella pagina ci sia la scritta giusta
