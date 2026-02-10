ab537b8a945afb7d519b0be725b0af2e56137ef6
Marco Zeolla <zeollamarco@gmail.com>
2026-02-10 18:25:35 +0100

    DevOps: Aggiunta Pipeline CI con Unit Tests automatici

---
+++ app/test_app.py
@@ -0,0 +1,24 @@
+import unittest
+from app import app
+
+class BasicTests(unittest.TestCase):
+
+    # 1. Preparazione del test
+    def setUp(self):
+        # Mettiamo l'app in modalità test
+        app.testing = True
+        self.app = app.test_client()
+
+    # 2. Il Test vero e proprio: Controlla se la Homepage risponde
+    def test_home_page_loads(self):
+        # Finge di andare su "/" (la home)
+        response = self.app.get('/')
+        
+        # Verifica che il codice di risposta sia 200 (OK)
+        self.assertEqual(response.status_code, 200)
+        
+        # Verifica che nella pagina ci sia la scritta giusta
+        self.assertIn(b"Markdown Converter", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
